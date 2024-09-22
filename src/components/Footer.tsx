e4b91f2009c200a7b14632d5841ad7392ea14ad1
Gmedulla101 <edosegheosamagbe@gmail.com>
2024-09-22 21:16:03 +0100

    Finalised profile UI

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,52 +1,87 @@
 const Footer = () => {
   return (
-    <section className="px-2 mt-10 lg:px-12 flex flex-col bg-gray-100 pb-12 md:justify-between md:items-center">
-      <div className="newsletter mb-8">
-        <h1 className="text-3xl font-semibold text-blue-600"> Houser </h1>
-        <h2 className="my-3 text-gray-600">
-          {' '}
-          Join our newsletter, we curate the best possible oppurtunities and
-          arrange them in an easily digestible way for your perusal
-        </h2>
-        <span className="flex gap-2">
-          <input
-            type="text"
-            name=""
-            id=""
-            placeholder="Enter your email"
-            className="outline-none border-2 border-gray-400 rounded-lg text-base p-2 w-full"
-          />
-          <button className="bg-blue-600 text-white px-4 py-2 rounded-lg">
+    <section className="px-2 mt-10 lg:px-12 flex flex-col bg-gray-100 pb-12 md:justify-between">
+      <h1 className="text-3xl font-semibold text-blue-600 mb-3 lg:text-4xl">
+        {' '}
+        Houser{' '}
+      </h1>
+
+      <section className="utilities flex flex-col md:justify-between">
+        <div className="newsletter mb-8">
+          <h2 className="my-3 text-gray-600">
             {' '}
-            Join{' '}
-          </button>
-        </span>
-      </div>
+            Join our newsletter, we curate the best possible oppurtunities and
+            arrange them in an easily digestible way for your perusal
+          </h2>
+          <span className="flex gap-2">
+            <input
+              type="text"
+              name=""
+              id=""
+              placeholder="Enter your email"
+              className="outline-none border-2 border-gray-400 rounded-lg text-base p-2 w-full"
+            />
+            <button className="bg-blue-600 text-white px-4 py-2 rounded-lg">
+              {' '}
+              Join{' '}
+            </button>
+          </span>
+        </div>
 
-      <div className="options w-full flex flex-col gap-5 md:flex-row md:justify-between">
-        <ul>
-          <li> About us </li>
-          <li> Our story </li>
-          <li> Our works </li>
-          <li> How it works </li>
-        </ul>
+        <div className="options text-sm w-full flex flex-col gap-5 md:flex-row md:justify-between md:px-6">
+          <ul>
+            <li className="hover:text-blue-600 cursor-pointer p-1">
+              {' '}
+              About us{' '}
+            </li>
+            <li className="hover:text-blue-600 cursor-pointer p-1">
+              Our story
+            </li>
+            <li className="hover:text-blue-600 cursor-pointer p-1">
+              Our works
+            </li>
+            <li className="hover:text-blue-600 cursor-pointer p-1">
+              How it works
+            </li>
+          </ul>
 
-        <ul>
-          <li> Properties </li>
-          <li> Wanna sell? </li>
-        </ul>
+          <ul>
+            <li className="hover:text-blue-600 cursor-pointer p-1">
+              Properties
+            </li>
+            <li className="hover:text-blue-600 cursor-pointer p-1">
+              {' '}
+              Wanna sell?{' '}
+            </li>
+          </ul>
 
-        <ul>
-          <li> Services </li>
-          <li> Property valuation </li>
-          <li> Personalised house hunting </li>
-          <li> Property management </li>
-        </ul>
+          <ul>
+            <li className="hover:text-blue-600 cursor-pointer p-1">
+              {' '}
+              Services{' '}
+            </li>
+            <li className="hover:text-blue-600 cursor-pointer p-1">
+              {' '}
+              Property valuation{' '}
+            </li>
+            <li className="hover:text-blue-600 cursor-pointer p-1">
+              {' '}
+              Personalised house hunting{' '}
+            </li>
+            <li className="hover:text-blue-600 cursor-pointer p-1">
+              {' '}
+              Property management{' '}
+            </li>
+          </ul>
 
-        <ul>
-          <li> Contact us </li>
-        </ul>
-      </div>
+          <ul>
+            <li className="hover:text-blue-600 cursor-pointer p-1">
+              {' '}
+              Contact us{' '}
+            </li>
+          </ul>
+        </div>
+      </section>
     </section>
   );
 };
--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -1,5 +1,56 @@
+import Header from '../components/Header';
+import Footer from '../components/Footer';
+
 const Dashboard = () => {
-  return <div>Dashboard</div>;
+  return (
+    <>
+      <Header />
+      <section className="px-4 mt-24">
+        <h1 className="text-3xl font-semibold">Edit Profile</h1>
+        <div className="editableDetails mt-8 flex flex-col gap-5">
+          <span>
+            <h3 className="text-xl font-semibold">Email</h3>
+            <span className="flex gap-8">
+              <p> johndoe@gmail.com </p>
+              <button className="text-blue-600 hover:underline">Edit</button>
+            </span>
+          </span>
+
+          <span>
+            <h3 className="text-xl font-semibold">Username</h3>
+            <span className="flex gap-8">
+              <p> JohhnyBoy </p>
+              <button className="text-blue-600 hover:underline">Edit</button>
+            </span>
+          </span>
+        </div>
+
+        <div className="basicDetails mt-12 flex flex-col gap-3">
+          <span>
+            <p className="text-xl font-semibold">Full name:</p>
+            <p> John Doe </p>
+          </span>
+
+          <span>
+            <p className="text-xl font-semibold">Phone number:</p>
+            <p> 08012312312 </p>
+          </span>
+
+          <span>
+            <p className="text-xl font-semibold">Location:</p>
+            <p> Edo State </p>
+          </span>
+        </div>
+
+        <div className="logout flex justify-center">
+          <button className="block text-sm text-center px-2 py-3 my-12 w-64  rounded-md bg-blue-600 text-white hover:scale-110 transition active:bg-blue-800 lg:px-6 lg:py-3">
+            Logout
+          </button>
+        </div>
+      </section>
+      <Footer />
+    </>
+  );
 };
 
 export default Dashboard;
