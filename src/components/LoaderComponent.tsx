883deb4d34b99c35cc737cc5ad9cee0ceba4e9e6
Gmedulla101 <edosegheosamagbe@gmail.com>
2024-10-07 17:19:20 +0100

    Added more flair to the loader component

--- src/components/LoaderComponent.tsx
+++ src/components/LoaderComponent.tsx
@@ -3,7 +3,7 @@ import { Loader } from '@mantine/core';
 const LoaderComponent = () => {
   return (
     <div className="flex flex-col justify-center items-center my-48">
-      <Loader size="100" color="gray" type="dots" />
+      <Loader size="100" color="blue" type="dots" />
       <p className="text-lg font-semibold"> Loading please wait... </p>
     </div>
   );
