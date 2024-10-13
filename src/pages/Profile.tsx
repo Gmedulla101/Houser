3d08e4798f0ecddf659a13566b1766a76436b973
Gmedulla101 <edosegheosamagbe@gmail.com>
2024-10-13 08:43:05 +0100

    Fixed profile access after sign out

--- src/pages/Profile.tsx
+++ src/pages/Profile.tsx
@@ -49,7 +49,7 @@ const Dashboard = () => {
   }, []);
 
   const navigate = useNavigate();
-  const { setIsSignedIn } = useGlobalContext();
+  const { setIsSignedIn, isSignedIn } = useGlobalContext();
 
   const logout = () => {
     localStorage.clear();
@@ -64,38 +64,46 @@ const Dashboard = () => {
         <LoaderComponent />
       ) : (
         <section className="px-4 mt-24">
-          <h1 className="text-3xl font-semibold">Edit Profile</h1>
-          <div className="editableDetails mt-8 flex flex-col gap-5">
-            <span>
-              <h3 className="text-xl font-semibold">Email</h3>
-              <span className="flex gap-8">
-                <p> {userData?.email} </p>
-              </span>
-            </span>
+          {!isSignedIn ? (
+            <h1 className="text-3xl font-semibold text-center my-48">
+              {' '}
+              You must be signed in to access this page{' '}
+            </h1>
+          ) : (
+            <>
+              {' '}
+              <h1 className="text-3xl font-semibold">Edit Profile</h1>
+              <div className="editableDetails mt-8 flex flex-col gap-5">
+                <span>
+                  <h3 className="text-xl font-semibold">Email</h3>
+                  <span className="flex gap-8">
+                    <p> {userData?.email} </p>
+                  </span>
+                </span>
 
-            <span>
-              <h3 className="text-xl font-semibold">Username</h3>
-              <span className="flex gap-8">
-                <p> {userData?.username} </p>
-              </span>
-            </span>
-          </div>
-
-          <div className="basicDetails mt-12 flex flex-col gap-3">
-            <span>
-              <p className="text-xl font-semibold">Full name:</p>
-              <p className="text-gray-600"> {userData?.fullName} </p>
-            </span>
-          </div>
-
-          <div className="logout flex justify-center">
-            <button
-              onClick={logout}
-              className="block text-sm text-center px-2 py-3 my-12 w-64  rounded-md bg-blue-600 text-white hover:scale-110 transition active:bg-blue-800 lg:px-6 lg:py-3"
-            >
-              Logout
-            </button>
-          </div>
+                <span>
+                  <h3 className="text-xl font-semibold">Username</h3>
+                  <span className="flex gap-8">
+                    <p> {userData?.username} </p>
+                  </span>
+                </span>
+              </div>
+              <div className="basicDetails mt-12 flex flex-col gap-3">
+                <span>
+                  <p className="text-xl font-semibold">Full name:</p>
+                  <p className="text-gray-600"> {userData?.fullName} </p>
+                </span>
+              </div>
+              <div className="logout flex justify-center">
+                <button
+                  onClick={logout}
+                  className="block text-sm text-center px-2 py-3 my-12 w-64  rounded-md bg-blue-600 text-white hover:scale-110 transition active:bg-blue-800 lg:px-6 lg:py-3"
+                >
+                  Logout
+                </button>
+              </div>
+            </>
+          )}
         </section>
       )}
       <Footer />
