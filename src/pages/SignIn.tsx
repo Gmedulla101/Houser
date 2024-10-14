7facf83c95846a3af97555e3f12ee91d86dfb2b3
Gmedulla101 <edosegheosamagbe@gmail.com>
2024-10-14 11:25:13 +0100

    Added loader component to the sign in page and ensured the enter key made the form submit

--- src/pages/SignIn.tsx
+++ src/pages/SignIn.tsx
@@ -7,6 +7,7 @@ import { useGlobalContext } from '../context/userContext';
 //IMPORITNG HELPER COMPONENTS
 import Header from '../components/Header';
 import Footer from '../components/Footer';
+import LoaderComponent from '../components/LoaderComponent';
 
 //IMPORTING IMAGE ASSETS
 import home from '../assets/home.png';
@@ -25,6 +26,7 @@ const SignIn = () => {
   const [errorMsg, setErrorMsg] = useState();
 
   const [isPassword, setIsPassword] = useState<boolean>(true);
+  const [isLoading, setIsLoading] = useState<boolean>(false);
 
   const handleFormChange = (e: any) => {
     const { name, value } = e.target;
@@ -43,6 +45,7 @@ const SignIn = () => {
       return;
     }
     try {
+      setIsLoading(true);
       const data = await axios.post(
         'https://houser-backend.onrender.com/api/v1/auth/user-login',
         {
@@ -72,6 +75,7 @@ const SignIn = () => {
       navigate('/');
     } catch (error: any) {
       console.error(error);
+      setIsLoading(false);
       setErrorMsg(error.response.data.msg);
     }
   };
@@ -79,59 +83,65 @@ const SignIn = () => {
   return (
     <>
       <Header />
-      <section className="mt-24 flex flex-col items-center">
-        <img src={home} alt="home" className="w-48 mb-12" />
 
-        <div className="errorPopup">
-          {' '}
-          {errorMsg ? (
-            <p className="border-2 border-red-400 bg-red-300 text-white font-semibold px-4 py-2 mb-2 rounded-lg transtion">
-              {errorMsg}
-            </p>
-          ) : (
-            ''
-          )}
-        </div>
+      {isLoading ? (
+        <LoaderComponent />
+      ) : (
+        <form className="mt-24 flex flex-col items-center">
+          <img src={home} alt="home" className="w-48 mb-12" />
 
-        <div className="signUpForm flex flex-col gap-5 items-center w-[90%] md:w-96">
-          <input
-            type="text"
-            name="email"
-            id="email"
-            placeholder="Enter your Email"
-            onChange={handleFormChange}
-            value={form.email}
-            className="border-2 border-gray-300 w-full py-2 px-4 rounded-lg outline-none focus:border-blue-600"
-          />
-
-          <div className="w-full flex items-center relative">
-            <input
-              type={isPassword ? 'password' : 'text'}
-              name="password"
-              id="password"
-              placeholder="Enter your password"
-              onChange={handleFormChange}
-              value={form.password}
-              className="border-2 border-gray-300 w-full py-2 px-4 rounded-lg outline-none focus:border-blue-600"
-            />
-            <span
-              className="absolute right-[10px] cursor-pointer"
-              onClick={() => {
-                setIsPassword(!isPassword);
-              }}
-            >
-              <img src={isPassword ? hidden : eye} alt="" className="w-7" />
-            </span>
+          <div className="errorPopup">
+            {' '}
+            {errorMsg ? (
+              <p className="border-2 border-red-400 bg-red-300 text-white font-semibold px-4 py-2 mb-2 rounded-lg transtion">
+                {errorMsg}
+              </p>
+            ) : (
+              ''
+            )}
           </div>
 
-          <button
-            onClick={handleSignIn}
-            className="block text-sm text-center px-2 py-3 w-[70%]  rounded-md bg-blue-600 text-white hover:scale-110 transition active:bg-blue-800 lg:px-6 lg:py-3"
-          >
-            Sign in
-          </button>
-        </div>
-      </section>
+          <div className="signUpForm flex flex-col gap-5 items-center w-[90%] md:w-96">
+            <input
+              type="text"
+              name="email"
+              id="email"
+              placeholder="Enter your Email"
+              onChange={handleFormChange}
+              value={form.email}
+              className="border-2 border-gray-300 w-full py-2 px-4 rounded-lg outline-none focus:border-blue-600"
+            />
+
+            <div className="w-full flex items-center relative">
+              <input
+                type={isPassword ? 'password' : 'text'}
+                name="password"
+                id="password"
+                placeholder="Enter your password"
+                onChange={handleFormChange}
+                value={form.password}
+                className="border-2 border-gray-300 w-full py-2 px-4 rounded-lg outline-none focus:border-blue-600"
+              />
+              <span
+                className="absolute right-[10px] cursor-pointer"
+                onClick={() => {
+                  setIsPassword(!isPassword);
+                }}
+              >
+                <img src={isPassword ? hidden : eye} alt="" className="w-7" />
+              </span>
+            </div>
+
+            <button
+              onClick={handleSignIn}
+              type="submit"
+              className="block text-sm text-center px-2 py-3 w-[70%]  rounded-md bg-blue-600 text-white hover:scale-110 transition active:bg-blue-800 lg:px-6 lg:py-3"
+            >
+              Sign in
+            </button>
+          </div>
+        </form>
+      )}
 
       <Footer />
     </>
