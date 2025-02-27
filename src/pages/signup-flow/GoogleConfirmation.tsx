3b616ccbd920a50b962551634cfa3f37b3c5660f
unknown <edosegheosamagbe@gmail.com>
2025-02-27 16:06:43 +0000

    Google auth complete on the front end

--- src/pages/signup-flow/GoogleConfirmation.tsx
+++ src/pages/signup-flow/GoogleConfirmation.tsx
@@ -1,4 +1,5 @@
 import Header from '../../components/Header';
+import { useEffect } from 'react';
 
 //IMPORTING IMAGE ASSETS
 import home from '../../assets/home.png';
@@ -10,10 +11,12 @@ import { Link } from 'react-router-dom';
 const GoogleConfirmation = () => {
   const [searchParams] = useSearchParams();
   const token: any = searchParams.get('token');
-
-  if(!token) {
-    throw new Error("Auth failure: Token is not present")
-  }
+  useEffect(() => {
+    if (!token) {
+      throw new Error('Auth failure: Token is not present');
+    }
+    localStorage.setItem('user', JSON.stringify(token));
+  }, []);
 
   return (
     <>
@@ -24,7 +27,7 @@ const GoogleConfirmation = () => {
             <img src={home} alt="Houser image" className="w-48" />
           </div>
           <h1 className="text-center font-bold text-3xl my-12">
-            You've successfully logged in{' '}
+            Welcome to Houser!
           </h1>
           <Link to={'/'}>
             <button className="block mx-auto bg-blue-600 py-3 px-6 rounded-lg text-white font-semibold transition hover:scale-105 active:bg-blue-700">
